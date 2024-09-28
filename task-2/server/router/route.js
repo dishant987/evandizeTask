ddd6d3bc048909f2dc63e0a85c1e53479af496fa
dishant987 <tankdishant2003@gmail.com>
2024-09-29 01:56:09 +0530

    change

--- task-2/server/router/route.js
+++ task-2/server/router/route.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import * as controller from "../controller/controller.js";
-import { verifyJWT } from "../middleware/auth.middleware.js";
 import { googleSignIn } from "../controller/googleController.js";
 const router = Router();
 
 router.route("/users/signup").post(controller.signUp);
 router.route("/users/google-signin").post(googleSignIn);
 router.route("/users/signin").post(controller.signIn);
-router.route("/users/logout").post(verifyJWT, controller.userLogout);
+router.route("/users/logout").post( controller.userLogout);
 
 export default router;
